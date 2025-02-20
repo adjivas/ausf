c96c6344642bfceb0d4ea107c5d6d427d7d12c6e
adjivas <adjivas@adjivas.eu>
2025-02-20 14:06:00 +0100

    refactor: add test for the RegisterAddr's utils function

--- internal/utils/net.go
+++ internal/utils/net.go
@@ -12,6 +12,9 @@ func RegisterAddr(registerIP string) netip.Addr {
 	if err != nil {
 		logger.InitLog.Errorf("Resolve RegisterIP hostname %s failed: %+v", registerIP, err)
 	}
-	ip, _ := netip.ParseAddr(ips[0].String());
+	ip, err := netip.ParseAddr(ips[0].String());
+	if err != nil {
+		logger.InitLog.Errorf("Parse Address %s failed: %+v", ips[0], err)
+	}
 	return ip
 }
